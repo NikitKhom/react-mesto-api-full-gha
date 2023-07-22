bb10efc0a08965a51a8771075cece34f6d4e25d5
Nikita Khomenko <nikita.khomenko.n25@gmail.com>
2023-07-22 11:44:14 +0300

    fixes/исправить работу logов

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const adminRouter = require('./routes/admin');
 const errorHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
@@ -28,3 +28,6 @@ app.use((req, res, next) => {
 app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
