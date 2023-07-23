ece0262ecb24b03b90aff2ab75c65de085e79521
Nikita Khomenko <nikita.khomenko.n25@gmail.com>
2023-07-23 12:25:53 +0300

    fixes/ исправить обработку CORS

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const auth = require('./middlewares/auth');
@@ -12,15 +11,16 @@ const errorHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+
 const app = express();
 
-app.use(cors());
 app.use(bodyParser.json());
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
 }).then(() => {
   console.log('connected to db');
 });
+app.use(cors());
 app.use(requestLogger);
 app.get('/crash-test', () => {
   setTimeout(() => {
